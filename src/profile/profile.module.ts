3c5c6e618aa94918e882a91444181f77f02da376
Nima Shahbazi <nimashahbazi524@gmail.com>
2023-05-17 15:27:49 +0330

    Fix build erros

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Files } from 'entities/FIles';
+import { UserProfile } from 'entities/UserProfile';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([File])],
+  imports: [TypeOrmModule.forFeature([Files, UserProfile])],
   providers: [ProfileService],
   controllers: [ProfileController]
 })

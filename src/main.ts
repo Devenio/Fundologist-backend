f50b627ee87f2d5e92c7bc5249c3e3657314ff70
Nima Shahbazi <nimashahbazi524@gmail.com>
2023-06-05 12:01:17 +0330

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fundologist Document')
+    .setDescription('The Fundologist API description')
+    .setVersion('1.0')
+    .addTag('fundologist')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      transform: true,  
+      transform: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(8080);
+
+  await app.listen(3001);
 }
 bootstrap();

39e05d8810d961abf3b36d098c2cede0fc99ec52
Nima Shahbazi <nimashahbazi524@gmail.com>
2023-06-16 02:16:29 +0330

    Use gaurd for add plan

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -1,12 +1,14 @@
 import { CreatePlanDto } from './create-plan.dto';
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { PlansService } from './plans.service';
 import { createOkResponse } from 'utils/createResponse';
+import { IsAdminGuard } from 'src/auth/guards/is-admin.guard';
 
 @Controller('plans')
 export class PlansController {
   constructor(private readonly plansService: PlansService) {}
 
+  @UseGuards(IsAdminGuard)
   @Post()
   async addPlan(@Body() createPlanDto: CreatePlanDto) {
     const plan = await this.plansService.create(createPlanDto);
